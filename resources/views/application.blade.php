7ce8c3e916543cee92aaa0223dc67e547c4c316b
dzoukou <dzoukouraoul@gmail.com>
2021-02-21 10:08:39 +0100

    commit

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,7 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- <link rel="icon" href="<%= BASE_URL %>favicon.ico"> -->
 
-  <title>Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template</title>
+  <title>BAV Test</title>
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 
